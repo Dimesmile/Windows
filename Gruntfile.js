3b632e7944dac76c9cdd016e080ffc05b18ff9f8
Dimesmile <Dimesmile@mail.ru>
2015-09-19 00:56:42 +0300

    becker init

---
+++ Gruntfile.js
@@ -0,0 +1,93 @@
+module.exports = function(grunt){
+	grunt.initConfig({
+		pkg: grunt.file.readJSON('package.json'),
+
+		jshint: {
+			options: {
+				curly: true,
+				eqeqeq: true,
+				immed: true,
+				latedef: true,
+				newcap: true,
+				sub: true,
+				undef: true,
+				eqnull: true,
+				browser: true,
+				globals: {
+					jQuery: true,
+					$: true,
+					console: true,
+				}
+			},
+			'<%= pkg.name %>': {
+				src: ['src/js/**/*.js']
+			}
+		},
+
+		concat: {
+			dist: {
+				src: ['bower_components/jquery/dist/jquery.min.js', 'bower_components/jquery-ui/jquery-ui.min.js', 'src/js/script.js'],
+				dest: 'www/build.js'
+			}
+		},
+
+		uglify: {
+			options: {
+				stripBanners: true,
+				banner: '/* <%= pkg.name %> - v<%= pkg.version %> - <%= grunt.template.today("yyyy-mm-dd") %>*/\n'
+			},
+			build: {
+				src: 'www/build.js',
+				dest: 'www/build.min.js'
+			}
+		},
+
+		cssmin: {
+			with_banner: {
+				options: {
+					banner: '/* My minified css */'
+				},
+
+				files: {
+					'www/style.min.css' : ['src/css/style.css']
+				}
+			}
+		},
+
+		htmlmin: {
+			dist: {                        
+      			options: {                                
+        			removeComments: true,
+        			collapseWhitespace: true
+      			},
+      			files: {                     
+        			'www/index.min.html': 'src/html/index.html' 
+      			}
+			}
+		},
+
+		watch: {
+			scripts: {
+				files: ['src/js/*.js'],
+				tasks: ['jshint', 'concat', 'uglify']
+			},
+			css: {
+				files: ['src/css/*.css'],
+				tasks: ['cssmin']
+			},
+			html: {
+				files: ['src/html/*.html'],
+				tasks: ['htmlmin']
+			}
+		}
+	});
+
+	grunt.loadNpmTasks('grunt-contrib-jshint');
+	grunt.loadNpmTasks('grunt-contrib-concat');
+	grunt.loadNpmTasks('grunt-contrib-uglify');
+	grunt.loadNpmTasks('grunt-contrib-cssmin');
+	grunt.loadNpmTasks('grunt-contrib-htmlmin');
+	grunt.loadNpmTasks('grunt-contrib-watch');
+
+	grunt.registerTask('default', ['jshint', 'concat', 'uglify', 'cssmin', 'htmlmin', 'watch']);
+};
---
+++ src/js/script.js
@@ -0,0 +1,42 @@
+$('#country-house').click(function(){
+	$('#country-house-up').show('slide',{direction: 'down'}, 400);
+});
+$('#country-house-up').mouseleave(function(){
+	$(this).hide('slide',{direction: 'down'}, 400);
+});
+
+$('#kitchen').click(function(){
+	$('#kitchen-up').show('slide',{direction: 'down'}, 400);
+});
+$('#kitchen-up').mouseleave(function(){
+	$(this).hide('slide',{direction: 'down'}, 400);
+});
+
+$('#baby-room').click(function(){
+	$('#baby-room-up').show('slide',{direction: 'down'}, 400);
+});
+$('#baby-room-up').mouseleave(function(){
+	$(this).hide('slide',{direction: 'down'}, 400);
+});
+
+$('#cottedge').click(function(){
+	$('#cottedge-up').show('slide',{direction: 'down'}, 400);
+});
+$('#cottedge-up').mouseleave(function(){
+	$(this).hide('slide',{direction: 'down'}, 400);
+});
+
+$('#office').click(function(){
+	$('#office-up').show('slide',{direction: 'down'}, 400);
+});
+$('#office-up').mouseleave(function(){
+	$(this).hide('slide',{direction: 'down'}, 400);
+});
+
+$('#balcony').click(function(){
+	$('#balcony-up').show('slide',{direction: 'down'}, 400);
+});
+$('#balcony-up').mouseleave(function(){
+	$(this).hide('slide',{direction: 'down'}, 400);
+});
+
